0821de155290ba8beee5a8bba49d1852ced3333b
RajRajputGit <brajrajput4@gmail.com>
2026-06-04 15:53:25 +0530

    Setup DVC pipeline stages: preprocess, train, evaluate; remove individual DVC tracking

---
+++ src/evaluate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+
+
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/brajrajputofficial/ml_pipeline_with_mlflow_projecy.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "brajrajputofficial"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "f01d1e3006e34deb71d36df5aaf8a49613c143a7"
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/brajrajputofficial/ml_pipeline_with_mlflow_projecy.mlflow")
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path,'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    mlflow.log_metric("accuracy",accuracy)
+    print(f"Model accuracy: {accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data_path"],params["model_path"])
